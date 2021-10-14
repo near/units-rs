4fc8036d39ecf5a00c541ee6fbf1a42fca75c3e0
Willem Wyndham <wyndham@cs.unc.edu>
2021-10-14 14:58:27 -0400

    fix: address feedback and simplify API

--- crates/units/src/lib.rs
+++ crates/units/src/lib.rs
@@ -1,23 +1,25 @@
 pub use near_units_core::*;
 pub use near_units_macro::*;
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
     const ONE_NEAR: u128 = 1_000_000_000_000_000_000_000_000;
     const ONE_TGAS: u128 = 1_000_000_000_000;
     const ONE_PARSED_NEAR: u128 = parse_near!("1 N");
-    const ONE_PARSED_TGAS: u128 = parse_gas!("1 TGas");
+    const ONE_PARSED_TGAS: u128 = parse_gas! ("1 TGas");
 
     #[test]
     fn const_parse_near() {
         assert_eq!(near::parse("1N").unwrap(), ONE_NEAR);
         assert_eq!(ONE_PARSED_NEAR, ONE_NEAR);
+
     }
 
     #[test]
     fn const_parse_gas() {
-        assert_eq!(gas::parse("1 TGas").unwrap(), ONE_TGAS);
-        assert_eq!(ONE_PARSED_TGAS, ONE_TGAS);
+      assert_eq!(gas::parse("1 TGas").unwrap(), ONE_TGAS);
+      assert_eq!(ONE_PARSED_TGAS, ONE_TGAS);
     }
 }
